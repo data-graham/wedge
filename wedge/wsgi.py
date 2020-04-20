384a74abeca111027d6d4ad1f49fb66cb2f0854d
Brian Tiemann <btman@mac.com>
2020-04-20 19:53:16 -0400

    patch

--- wedge/wsgi.py
+++ wedge/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for thinkpackage project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os, sys
 from django.core.wsgi import get_wsgi_application
 
